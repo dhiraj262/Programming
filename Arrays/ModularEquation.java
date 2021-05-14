662955e645cd4ab2e775104f4869b732a9cd49ca
Dhiraj Kumar <31141888+dhiraj262@users.noreply.github.com>
2021-05-15 00:03:13 +0530

    modular equation

---
+++ Arrays/ModularEquation.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.math.BigInteger;
+/*
+Given integers N and M, find the number of ordered pairs (a,b) such that 1≤a<b≤N and ((M mod a) mod b)=((M mod b) mod a).
+*/
+public class ModularEquation {
+
+	public static BigInteger modEqn(int n,int m){
+		BigInteger[] modArray = new BigInteger[n+1];
+		for(int i=0;i<=n;i++){
+			modArray[i] = BigInteger.ONE;
+		}
+		BigInteger count=BigInteger.ZERO;
+		    for(int i=2;i<=n;i++){
+		    	int x = BigInteger.valueOf((long)m).mod(BigInteger.valueOf((long)i)).intValue();
+			
+			    count = count.add(modArray[x]);
+			    for(int j=x;j<=n;j+=i){
+				    modArray[j]= modArray[j].add(BigInteger.ONE);
+			    }
+		}
+		return count;
+	}
+	ArrayList<Integer> array = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    long count = 0;
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    for(int i=1;i<=n;i++){
+		        for(int j=i+1;j<=n;j++){
+		            if((m%i)%j == (m%j)%i){
+                        //System.out.println(i + " "+ j);
+                        
+		                count++;
+		            }
+                }
+		    }
+		    System.out.println(count);
+			System.out.println(modEqn(n, m));
+		}
+    }
+}
