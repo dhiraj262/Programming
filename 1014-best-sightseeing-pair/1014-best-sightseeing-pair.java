1a713f010c92727f1963a33efc89ab27696f0bad
Dhiraj Kumar <31141888+dhiraj262@users.noreply.github.com>
2022-03-03 12:03:35 +0530

    Update 1014-best-sightseeing-pair.java

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -3,6 +3,7 @@ class Solution {
         int ans = 0, curr = 0;
         for (int v: values) {
             ans = Math.max(ans, curr + v);
+            // when we move forward,all sightseeing spot we have seen will be 1 distance further.
             curr = Math.max(curr, v) - 1;
         }
         return ans;
