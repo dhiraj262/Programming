cc8f69f6db463f27b70270a8eb712f67f7bc6abc
Dhiraj Kumar <31141888+dhiraj262@users.noreply.github.com>
2022-03-08 23:19:44 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (58.77%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numTrees(int n) {
+          int [] bst = new int[n+1];
+          bst[0] = bst[1] = 1;
+
+          for(int i=2; i<=n; ++i) {
+            for(int j=1; j<=i; ++j) {
+              bst[i] += bst[j-1] * bst[i-j];
+            }
+          }
+          return bst[n];
+    }
+}
